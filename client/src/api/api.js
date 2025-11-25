cf178167953a0aab15dcda4ed70f73d2df60a544
tushar chauhan <tusharchauhan120104@zohomail.in>
2025-11-25 22:58:29 +0530

    save

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://resumebuild-k908.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
