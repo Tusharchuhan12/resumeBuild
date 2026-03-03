e7c63e6086afc123044fe373c6765cf4c4adc5ec
tushar chauhan <tusharchauhan120104@zohomail.in>
2026-03-04 00:04:08 +0530

    fix

--- client/src/Components/CallTo.jsx
+++ client/src/Components/CallTo.jsx
@@ -73,14 +73,20 @@ export default function CallTo() {
                     </p>
 
                     {/* Call to Action Button */}
-                    <div className="rainbow relative z-0 bg-white/15 overflow-hidden p-0.5 flex items-center justify-center rounded-full hover:scale-105 transition duration-300 active:scale-100 mt-6">
+                    <div className=" gap-3 rainbow relative z-0 bg-white/15 overflow-hidden p-0.5 flex items-center justify-center rounded-full hover:scale-105 transition duration-300 active:scale-100 mt-6">
                         <button
                             className="px-8 text-sm py-3 text-white rounded-full font-medium bg-gray-900/80 backdrop-blur"
                             onClick={() => navigate("/layout")}
                         >
                             View My Resume
                         </button>
-
+                       
+                        <button
+                            className="px-8 text-sm py-3 text-white rounded-full font-medium bg-gray-900/80 backdrop-blur"
+                            onClick={() => window.open("https://ai-interview-agent-zeta.vercel.app/", "_blank")}
+                        >
+                            Ai Interview Agent
+                        </button>
                     </div>
                 </div>
             </div>
